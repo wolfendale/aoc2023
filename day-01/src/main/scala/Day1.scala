14c2049e4a99383338669d45a5f5061eaba23e9d
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2023-12-01 13:54:06 +0000

    Day 1 part 2

--- day-01/src/main/scala/Day1.scala
+++ day-01/src/main/scala/Day1.scala
@@ -1,12 +1,19 @@
+import atto.Parser
 import cats.data.NonEmptyList
+
+import cats._
+import cats.implicits._
 
 object Day1 {
 
-  def part1(values: String): Int =
-    Parser.parse(values)
+  def part1(input: String): Int =
+    Part1Parser.parse(input)
+
+  def part2(input: String): Int =
+    Part2Parser.parse(input)
 }
 
-object Parser {
+object Part1Parser {
 
   import atto._
   import Atto._
@@ -19,6 +26,52 @@ object Parser {
 
   private val calibrationDigit: Parser[Char] =
     letter.many ~> digit
+
+  private val calibrationNumber: Parser[Int] =
+    (calibrationDigit.many1 <~ letter.many).map { chars =>
+      s"${chars.head}${chars.last}".toInt
+    }
+
+  private val parser: Parser[Int] =
+    calibrationNumber.sepBy1(newline).map(_.reduceLeft(_ + _)) <~ endOfInput
+}
+
+object Part2Parser {
+
+  import atto._
+  import Atto._
+
+  def parse(input: String): Int =
+    parser.parseOnly(input).done.option.get
+
+  def lookahead[A](parser: Parser[A]): Parser[A] =
+    (pos ~ parser ~ pos).flatMap { case ((start, value), end) =>
+      advance(-(end - start)) >| value
+    }
+
+  private val newline: Parser[Char] =
+    char('\r') | char('\n')
+
+  private val wordDigit: Parser[Char] =
+    lookahead(
+      digit |
+      (string("zero") >| '0') |
+      (string("one") >| '1') |
+      (string("two") >| '2') |
+      (string("three") >| '3') |
+      (string("four") >| '4') |
+      (string("five") >| '5') |
+      (string("six") >| '6') |
+      (string("seven") >| '7') |
+      (string("eight") >| '8') |
+      (string("nine") >| '9')
+    ) <~ advance(1)
+
+  private lazy val unwantedLetters: Parser[Unit] =
+    manyUntil(letter, lookahead(wordDigit)).void
+
+  private val calibrationDigit: Parser[Char] =
+    unwantedLetters ~> wordDigit
 
   private val calibrationNumber: Parser[Int] =
     (calibrationDigit.many1 <~ letter.many).map { chars =>
--- day-01/src/test/scala/Day1Test.scala
+++ day-01/src/test/scala/Day1Test.scala
@@ -5,22 +5,42 @@ import scala.io.Source
 
 class Day1Test extends AnyFreeSpec with Matchers {
 
-  private val testInput =
+  private val testInput1 =
     """1abc2
       |pqr3stu8vwx
       |a1b2c3d4e5f
       |treb7uchet""".stripMargin
+
+  private val testInput2 =
+    """two1nine
+      |eightwothree
+      |abcone2threexyz
+      |xtwone3four
+      |4nineeightseven2
+      |zoneight234
+      |7pqrstsixteen""".stripMargin
 
   private val input = Source.fromResource("input.txt").mkString
 
   "part 1" - {
 
     "must succeed for the example input" in {
-      Day1.part1(testInput) mustEqual 142
+      Day1.part1(testInput1) mustEqual 142
     }
 
     "must succeed for the test input" in {
-      Day1.part1(input) mustEqual 0
+      Day1.part1(input) mustEqual 54927
+    }
+  }
+
+  "part 2" - {
+
+    "must succeed for the example input" in {
+      Day1.part2(testInput2) mustEqual 281
+    }
+
+    "must succeed for the test input" in {
+      Day1.part2(input) mustEqual 0
     }
   }
 }
